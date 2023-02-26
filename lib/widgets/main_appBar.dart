850f1a0ed95c5dda0890d9716f2be33d56aed4eb
Hyeeun Shin <as221323@kookmin.ac.kr>
2023-02-26 20:28:57 +0900

    add main_appbar(upper navigation bar)

---
+++ lib/widgets/main_appBar.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class MainAppBar extends StatefulWidget {
+  const MainAppBar({super.key});
+  @override
+  State<MainAppBar> createState() => _MainAppBarState();
+}
+
+class _MainAppBarState extends State<MainAppBar>
+    with SingleTickerProviderStateMixin {
+  static const List<Tab> myTabs = <Tab>[
+    Tab(text: '질문'),
+    Tab(text: '피드'),
+    Tab(text: '메시지'),
+    Tab(
+      text: '확인용',
+      icon: Icon(Icons.settings),
+    ),
+  ];
+
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: myTabs.length);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xffFFFFFF),
+        flexibleSpace: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            TabBar(
+                controller: _tabController,
+                labelStyle: const TextStyle(
+                    fontSize: 18.0, fontWeight: FontWeight.bold),
+                labelColor: Colors.black,
+                unselectedLabelColor: Colors.grey,
+                tabs: myTabs),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: myTabs.map((Tab tab) {
+          final String label = tab.text!.toLowerCase();
+          return Center(
+            child: Text(
+              'This is the $label tab',
+              style: const TextStyle(fontSize: 35),
+            ),
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
