22d037d2a4b509caace3e108098ec68cd7900f41
Baek-sohyeon <kitty224@kookmin.ac.kr>
2023-03-07 11:53:12 +0900

    kakao-login-page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cooing_front/pages/main_page.dart';
+import 'package:cooing_front/pages/kakaoLogin_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: MainPage(),
+      home: KakaoLoginPage(),
     );
   }
 }
---
+++ lib/pages/kakaoLogin_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class KakaoLoginPage extends StatefulWidget {
+  const KakaoLoginPage({super.key});
+
+  @override
+  _KakaoLoginPageState createState() => _KakaoLoginPageState();
+}
+
+class _KakaoLoginPageState extends State<KakaoLoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        backgroundColor: Color(0xFFffffff),
+        body: Container(
+          padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+          child: Form(
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Text(
+                "당신을 몰래 좋아하는",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22,
+                    color: Color.fromARGB(255, 51, 61, 75)),
+              ),
+              Text(
+                "사람은 누굴까요?",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22,
+                    color: Color.fromARGB(255, 51, 61, 75)),
+              ),
+              Spacer(),
+              SizedBox(
+                  width: double.infinity,
+                  child: TextButton(
+                    child: Image.asset('images/kakao_login_medium_wide.png'),
+                    onPressed: () {},
+                  ))
+            ]),
+          ),
+        ));
+  }
+}
