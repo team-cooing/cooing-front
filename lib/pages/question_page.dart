53f105b2e2bb8037340cacad23c9d71bec02603c
Hyeeun Shin <as221323@kookmin.ac.kr>
2023-03-09 17:11:12 +0900

    create function(get question) *question_page.dart

--- lib/pages/question_page.dart
+++ lib/pages/question_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import "dart:math";
 
 class QuestionPage extends StatefulWidget {
   const QuestionPage({super.key});
@@ -8,6 +9,28 @@ class QuestionPage extends StatefulWidget {
 }
 
 class _QuestionPageState extends State<QuestionPage> {
+  String checking = '<버튼누름>';
+  String askDefault = '똑똑똑! 오늘의 질문이 도착했어요.';
+  String getAsk = '질문 받기';
+  String getAnswer = '답변 받기';
+  String closeAsk = '질문 닫기';
+
+  late String askText = askDefault;
+  late String askButtonText = getAsk;
+
+  var questionList = ['내 첫인상은 어땠어?', '내 mbti는 무엇인 것 같아?', '나랑 닮은 동물은 뭐야?'];
+
+  final _random = Random();
+
+  changeAskCard() {
+    var question = questionList[_random.nextInt(questionList.length)];
+
+    setState(() {
+      askText = question;
+      askButtonText = getAnswer;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,52 +39,48 @@ class _QuestionPageState extends State<QuestionPage> {
   }
 
   Widget _askBody() {
-    const String checking = '<버튼누름>';
-    const String askNotice = '똑똑똑! 오늘의 질문이 도착했어요.';
-    const String askGet = '질문 받기';
+    // List<Widget> _askCard(List myCard) {
+    //   var list = myCard.map<List<Widget>>(
+    //     (data) {
+    //       var widgetList = <Widget>[];
+    //       widgetList.add(const Padding(padding: EdgeInsets.all(8.0)));
+    //       widgetList.add(const SizedBox(
+    //           width: 80.0,
+    //           height: 80.0,
+    //           child: CircleAvatar(
+    //             backgroundImage: AssetImage('images/sohee.jpg'),
+    //           )));
+    //       widgetList.add(const Padding(padding: EdgeInsets.all(10.0)));
+    //       widgetList.add(Text(
+    //         askDefault,
+    //         style: const TextStyle(
+    //             fontWeight: FontWeight.bold, fontSize: 16, color: Colors.white),
+    //       ));
+    //       widgetList.add(const Padding(padding: EdgeInsets.all(20.0)));
+    //       widgetList.add(OutlinedButton(
+    //         onPressed: () => print(checking),
+    //         style: OutlinedButton.styleFrom(
+    //           fixedSize: const Size(154, 50),
+    //           foregroundColor: const Color(0xff9754FB),
+    //           backgroundColor: Colors.white,
+    //           shape: RoundedRectangleBorder(
+    //             borderRadius: BorderRadius.circular(30),
+    //           ),
+    //         ),
+    //         child: Text(
+    //           getAsk,
+    //           style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+    //         ),
+    //       ));
+    //       widgetList.add(const Padding(padding: EdgeInsets.all(10.0)));
 
-    List<Widget> _askCard(List myCard) {
-      var list = myCard.map<List<Widget>>(
-        (data) {
-          var widgetList = <Widget>[];
-          widgetList.add(const Padding(padding: EdgeInsets.all(8.0)));
-          widgetList.add(const SizedBox(
-              width: 80.0,
-              height: 80.0,
-              child: CircleAvatar(
-                backgroundImage: AssetImage('images/sohee.jpg'),
-              )));
-          widgetList.add(const Padding(padding: EdgeInsets.all(10.0)));
-          widgetList.add(const Text(
-            askNotice,
-            style: TextStyle(
-                fontWeight: FontWeight.bold, fontSize: 16, color: Colors.white),
-          ));
-          widgetList.add(const Padding(padding: EdgeInsets.all(20.0)));
-          widgetList.add(OutlinedButton(
-            onPressed: () => print(checking),
-            style: OutlinedButton.styleFrom(
-              fixedSize: const Size(154, 50),
-              foregroundColor: const Color(0xff9754FB),
-              backgroundColor: Colors.white,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
-            ),
-            child: const Text(
-              askGet,
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-            ),
-          ));
-          widgetList.add(const Padding(padding: EdgeInsets.all(10.0)));
+    //       return widgetList;
+    //     },
+    //   ).toList();
 
-          return widgetList;
-        },
-      ).toList();
-
-      var flat = list.expand((i) => i).toList();
-      return flat;
-    }
+    //   var flat = list.expand((i) => i).toList();
+    //   return flat;
+    // }
 
     return Padding(
         padding: const EdgeInsets.all(15.0),
@@ -88,16 +107,16 @@ class _QuestionPageState extends State<QuestionPage> {
                       ),
                     ),
                     const Padding(padding: EdgeInsets.all(10.0)),
-                    const Text(
-                      askNotice,
-                      style: TextStyle(
+                    Text(
+                      askText,
+                      style: const TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 16,
                           color: Colors.white),
                     ),
                     const Padding(padding: EdgeInsets.all(20.0)),
                     OutlinedButton(
-                      onPressed: () => print(checking),
+                      onPressed: () => changeAskCard(),
                       style: OutlinedButton.styleFrom(
                         fixedSize: const Size(154, 50),
                         foregroundColor: const Color(0xff9754FB),
@@ -106,9 +125,9 @@ class _QuestionPageState extends State<QuestionPage> {
                           borderRadius: BorderRadius.circular(30),
                         ),
                       ),
-                      child: const Text(
-                        askGet,
-                        style: TextStyle(
+                      child: Text(
+                        askButtonText,
+                        style: const TextStyle(
                             fontWeight: FontWeight.bold, fontSize: 16),
                       ),
                     ),
