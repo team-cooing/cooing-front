37a015942b7adeb6392ce089f8c86a57f87f4651
Hyeeun Shin <as221323@kookmin.ac.kr>
2023-03-15 18:53:31 +0900

    modify answer_page ui

--- lib/pages/answer_page.dart
+++ lib/pages/answer_page.dart
@@ -10,62 +10,73 @@ class AnswerPage extends StatefulWidget {
 class _AnswerPageState extends State<AnswerPage> {
   var questionList = ['내 첫인상은 어땠어?', '내 mbti는 무엇인 것 같아?', '나랑 닮은 동물은 뭐야?'];
   String askText = '내 첫인상은 어땠어?';
-  final bool _checkSecret = false;
+  bool? _checkSecret = false;
+  int maxLength = 100;
+  String textValue = "";
+  final TextEditingController _textController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
-        leading: const IconButton(
-          icon: Icon(Icons.close),
-          onPressed: null,
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          leading: const IconButton(
+            icon: Icon(Icons.close),
+            onPressed: null,
+          ),
         ),
-      ),
-      body: _answerBody(),
-    );
+        body: SafeArea(child: _answerBody()),
+        bottomSheet: SafeArea(
+          child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Padding(
+                padding: EdgeInsets.only(
+                  bottom:
+                      MediaQuery.of(context).systemGestureInsets.bottom + 15,
+                ),
+                child: sendBtn())
+          ]),
+        ));
   }
 
   Widget _answerBody() {
-    return Column(
+    return SingleChildScrollView(
+        child: Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const Row(
-          children: [
-            Padding(padding: EdgeInsets.all(15.0)),
-            Text(
-              "답변 작성",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 22.0,
-              ),
-              textAlign: TextAlign.center,
-            )
-          ],
+        const SingleChildScrollView(
+          child: Row(
+            children: [
+              Padding(padding: EdgeInsets.all(15.0)),
+              Text(
+                "답변 작성",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22.0,
+                ),
+                textAlign: TextAlign.center,
+              )
+            ],
+          ),
         ),
         const Padding(padding: EdgeInsets.all(7.0)),
         _answerCard(),
         checking(),
-        const Padding(padding: EdgeInsets.all(85.0)),
-        sendBtn()
       ],
-    );
+    ));
   }
 
   Widget _answerCard() {
-    const int maxLength = 100;
-    String textValue = "";
-
     return Center(
         child: SizedBox(
             width: 346.0,
-            height: 346.0,
+            height: 500.0,
             child: Card(
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(30)),
               color: const Color(0xff9754FB),
               child: Column(children: <Widget>[
-                const Padding(padding: EdgeInsets.all(8.0)),
+                const Padding(padding: EdgeInsets.all(15.0)),
                 const SizedBox(
                   width: 80.0,
                   height: 80.0,
@@ -82,35 +93,50 @@ class _AnswerPageState extends State<AnswerPage> {
                       color: Colors.white),
                 ),
                 const Padding(padding: EdgeInsets.all(15.0)),
-                SizedBox(
-                    width: 300,
-                    height: 125,
-                    child: TextField(
-                      textAlign: TextAlign.left,
-                      expands: true,
-                      maxLines: null,
-                      maxLength: maxLength,
-                      onChanged: (value) {
-                        setState(() {
-                          textValue = value;
-                        });
-                      },
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                              borderSide: const BorderSide(
-                                  width: 0, style: BorderStyle.none)),
-                          contentPadding: const EdgeInsets.all(20),
-                          filled: true,
-                          fillColor: Colors.white24,
-                          hintText: '당신의 진심을 담은 답변을 적어주세요',
-                          hintStyle: const TextStyle(
-                              fontSize: 14, color: Colors.white38),
-                          counterText: "",
-                          suffix: Text("${textValue.length} / $maxLength")),
-                    )),
+                answerTxtField(),
               ]),
             )));
+  }
+
+  Widget answerTxtField() {
+    // String textLength = "0 / maxLength";
+
+    return SizedBox(
+        width: 300,
+        height: 200,
+        child: Column(children: [
+          TextField(
+            controller: _textController,
+            maxLines: 5,
+            maxLength: 100,
+            onChanged: (value) {
+              setState(() {
+                textValue = value;
+              });
+            },
+            decoration: InputDecoration(
+              counterText: "",
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide:
+                      const BorderSide(width: 0, style: BorderStyle.none)),
+              contentPadding: const EdgeInsets.all(10),
+              filled: true,
+              fillColor: Colors.white24,
+              hintText: '당신의 진심을 담은 답변을 적어주세요',
+              hintStyle: const TextStyle(
+                fontSize: 14,
+                color: Colors.white38,
+              ),
+            ),
+          ),
+          SizedBox(
+              height: 14,
+              child: Text(
+                "\n\n\n\n\n${textValue.length} / $maxLength",
+                style: const TextStyle(color: Colors.white54, fontSize: 12),
+              ))
+        ]));
   }
 
   Widget checking() {
@@ -118,12 +144,16 @@ class _AnswerPageState extends State<AnswerPage> {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Transform.scale(
-          scale: 1.5,
+          scale: 1.4,
           child: Checkbox(
-            activeColor: Colors.amberAccent,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+            activeColor: const Color(0xff9754FB),
             value: _checkSecret,
             onChanged: (value) {
-              setState(() {});
+              setState(() {
+                _checkSecret = value;
+              });
             },
           ),
         ),
@@ -136,24 +166,26 @@ class _AnswerPageState extends State<AnswerPage> {
   }
 
   Widget sendBtn() {
-    return Center(
-        child: SizedBox(
-            width: 350,
-            height: 60,
-            child: ElevatedButton(
-                onPressed: null,
-                style: OutlinedButton.styleFrom(
-                  foregroundColor: Colors.white,
-                  backgroundColor: const Color(0xff9754FB),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0)),
-                ),
-                child: const Text(
-                  "보내기",
-                  style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
-                ))));
+    return Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+      SizedBox(
+        width: 350,
+        height: 60,
+        child: ElevatedButton(
+            onPressed: null,
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: const Color(0xff9754FB),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0)),
+            ),
+            child: const Text(
+              "보내기",
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            )),
+      )
+    ]);
   }
 }
