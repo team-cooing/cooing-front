3d1c858e16e362f16dbee2bb5819bb599417a89f
ureChanger <rlfgus8708@gmail.com>
2023-05-09 15:01:34 +0900

    [Created] getCurrentQuestionData()

--- lib/pages/tab_page.dart
+++ lib/pages/tab_page.dart
@@ -1,3 +1,4 @@
+import 'package:cooing_front/model/response/question.dart';
 import 'package:cooing_front/model/response/user.dart';
 import 'package:cooing_front/pages/SettingScreen.dart';
 import 'package:cooing_front/pages/login/LoginScreen.dart';
@@ -17,9 +18,11 @@ class TabPage extends StatefulWidget {
 }
 
 class TabPageState extends State<TabPage> with TickerProviderStateMixin {
+  late TabController _tabController;
   late String uid = '';
   late User? user;
-  late TabController _tabController;
+  late Question? currentQuestion;
+  bool isUserDataGetting = true;
   bool isLoading = true;
 
   List<Tab> myTabs = <Tab>[
@@ -39,10 +42,10 @@ class TabPageState extends State<TabPage> with TickerProviderStateMixin {
 
     // 1. 인자로 전달 받은 uid 가져오기
     uid = Get.arguments.toString();
-    print('TabPage: $uid');
+    print('로그인 유저 UID: $uid');
 
-    // 2. Firebase에서 User Data 가져오기
-    getUserFromFirebase();
+    // 2. Firebase에서 Data 가져오기
+    getDataFromFirebase();
   }
 
   @override
@@ -99,18 +102,51 @@ class TabPageState extends State<TabPage> with TickerProviderStateMixin {
     );
   }
 
-  getUserFromFirebase() async{
-    // Firebase DB에서 User 읽기
-    user = await response.Response.readUser(userUid: uid);
+  getDataFromFirebase() async{
+    // 1. User 데이터 가져오기
+    user = await getUserData();
 
     // 만약, 유저가 있다면
     if(user!=null){
-      setState(() {
-        isLoading = false;
-      });
+      // 2. Current Question 데이터 가져오기
+      currentQuestion = await getCurrentQuestionData();
+
+      // TODO: 3. Feed Questions 데이터 가져오기
+
     }else{
       // 로그인 페이지로 이동
       Get.offAll(LoginScreen());
+    }
+
+
+  }
+
+  getUserData() async{
+    // Firebase DB에서 User 읽기
+    User? newUser = await response.Response.readUser(userUid: uid);
+
+    return newUser;
+  }
+
+  getCurrentQuestionData() async{
+    // 만약, currentQuestionId가 있다면
+    if(user!.currentQuestionId.isNotEmpty){
+      String currentQuestionId = user!.currentQuestionId;
+
+      // currentQuestionId = '#{contentId}_{questionId}'
+      // currentQuestionId를 contentId와 questionId로 분리
+      String strippedVariable = currentQuestionId.replaceAll('#', ''); // # 제거
+      List<String> ids = strippedVariable.split('_'); // _로 분리
+
+      String contentId = ids[0];
+      String questionId = ids[1];
+
+      // Firebase DB에서 Question 읽기
+      Question? newQuestion = await response.Response.readQuestion(contentId: contentId, questionId: questionId);
+
+      return newQuestion;
+    }else{
+      return null;
     }
   }
 }
