9d34f8894be049d95fb2472e324ffbfd03d80f03
Baek-sohyeon <kitty224@kookmin.ac.kr>
2023-05-25 16:07:11 +0900

    Fix all

--- lib/pages/login/AgreeScreen.dart
+++ lib/pages/login/AgreeScreen.dart
@@ -7,6 +7,7 @@ import 'package:url_launcher/url_launcher.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart' as kakao;
 import 'package:firebase_auth/firebase_auth.dart' as firebase;
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AgreeScreen extends StatefulWidget {
   const AgreeScreen({super.key});
@@ -96,7 +97,8 @@ class _AgreeScreenState extends State<AgreeScreen> {
                           "https://we-cooing.notion.site/9a8b8ef68b964b5881c57ce50657854d");
 
                       if (await canLaunchUrl(reportUrl)) {
-                        launchUrl(reportUrl);
+                        launchUrl(reportUrl,
+                            mode: LaunchMode.externalApplication);
                       } else {
                         // ignore: avoid_print
                         print("Can't launch $reportUrl");
@@ -141,7 +143,8 @@ class _AgreeScreenState extends State<AgreeScreen> {
                           "https://we-cooing.notion.site/909def3b7b194ac48fabe0f142f60176");
 
                       if (await canLaunchUrl(reportUrl)) {
-                        launchUrl(reportUrl);
+                        launchUrl(reportUrl,
+                            mode: LaunchMode.externalApplication);
                       } else {
                         // ignore: avoid_print
                         print("Can't launch $reportUrl");
