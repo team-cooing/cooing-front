1cd4bdc219ef945040f937dab064fda59f364bc5
Baek-sohyeon <kitty224@kookmin.ac.kr>
2023-06-20 11:51:40 +0900

    뭐가 이렇게 많이 추가되었을까

---
+++ ios/build/Pods.build/Debug-iphonesimulator/abseil copy.build/DerivedSources/abseil copy_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char abseil_copyVersionString[];
+ extern const double abseil_copyVersionNumber;
+
+ const unsigned char abseil_copyVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:abseil copy  PROJECT:Pods-1" "\n";
+ const double abseil_copyVersionNumber __attribute__ ((used)) = (double)1.;
